8905b59c9c598c5fcbcb8a7351650878e3e7f010
18352978082 <1091123779@qq.com>
2026-05-15 21:50:19 +0800

    新增环境配置config.prod.js和config.local.js

---
+++ config/config.local.js
@@ -0,0 +1,9 @@
+module.exports = () => {
+    const config = {};
+
+    config.mongoose = {
+        url: process.env.MONGO_URL || 'mongodb://127.0.0.1/ms-da-projects',
+    };
+
+    return config;
+};
